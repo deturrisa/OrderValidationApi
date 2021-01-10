b3cbc43794c8a137e3444bd46df53c12b2951e91
Alex de Turris <alex@playstudios.asia>
2021-01-10 18:52:04 +0800

    implementation of ClientValidationFactory

--- OrderValidation.Client/ClientValidationFactory.cs
+++ OrderValidation.Client/ClientValidationFactory.cs
@@ -1,14 +1,32 @@
 ﻿using System;
-using OrderValidation.Client.Clients;
+using System.Collections.Generic;
 using OrderValidation.Client.Clients.Interfaces;
 
 namespace OrderValidation.Client
 {
     public class ClientValidationFactory : IClientValidationFactory
     {
+        private readonly IDictionary<string, Func<IClientValidation>> _clientValidationDictionary;
+
+        public ClientValidationFactory(Lazy<IClientAValidationService> clientAValidationService,
+            Lazy<IClientBValidationService> clientBValidationService,
+            Lazy<IClientCValidationService> clientCValidationService)
+        {
+            _clientValidationDictionary = new Dictionary<string, Func<IClientValidation>>
+            {
+                {"A",() => clientAValidationService.Value },
+                {"B",() => clientBValidationService.Value },
+                {"C",() => clientCValidationService.Value }
+            };
+        }
         public IClientValidation GetClientValidation(string clientId)
         {
-            throw new ArgumentException();
+            if (!_clientValidationDictionary.TryGetValue(clientId, out var getProvider))
+            {
+                throw new ArgumentOutOfRangeException($"Invalid ClientId '{clientId}'");
+            }
+
+            return getProvider();
         }
     }
 }
